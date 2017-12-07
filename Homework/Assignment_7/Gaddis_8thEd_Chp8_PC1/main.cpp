c85a1e6e4d2b052ba87cd0372ecd9d6cf7462f3d
Cory Chesebro <cchesebro1@student.rccd.edu>
2017-12-07 15:55:49 -0800

    Signed-off-by: Cory Chesebro <cchesebro1@student.rccd.edu>

--- Homework/Assignment_7/Gaddis_8thEd_Chp8_PC1/main.cpp
+++ Homework/Assignment_7/Gaddis_8thEd_Chp8_PC1/main.cpp
@@ -22,12 +22,12 @@ using namespace std;
 int main() {
     //Declare variables
     int size = 18;
-    int *accnts = new int [size];
+    int *accnts = new int [size] {5658845,4520125,7895122,877541,8451277,13850,
+    8080152,4562555,5552012,5050552,7825877,1250255,1005231,6545231,3852085,
+    7576651,7881200,4581002};
     
     //Initialize variables
-    *accnts = 5658845,4520125,7895122,877541,8451277,13850,8080152,4562555,
-    5552012,5050552,7825877,1250255,1005231,6545231,3852085,7576651,7881200,
-    4581002;
+
     
     //Process information
     cout<<"Enter the account number you wish to charge: ";
@@ -35,13 +35,13 @@ int main() {
     cin>>temp;
     
     for(short i = 0; i < size; i++){
-        if(temp == accnts[i]){
+        if(accnts[i] == temp ){
             cout<<"Account successfully charged"<<endl;
-        }
-        else{
-            cout<<"That account doesn't exist, try again."<<endl;break;
+            exit(EXIT_FAILURE);
         }
     }
+    cout<<"That account doesn't exist, try again."<<endl;
+        
     
     //Exit to function main / End program
     return 0;
